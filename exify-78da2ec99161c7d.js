5601721a4aaba088c43cf655fe7629414b9e5b72
sectore <sectore@users.noreply.github.com>
2023-11-11 08:31:24 +0000

    deploy: dd045b53290f5daf7489d0330a61e560c55ba7e6

--- exify-78da2ec99161c7d.js
+++ exify-78da2ec99161c7d.js
@@ -800,7 +800,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('exify-d37eaef931a849c2_bg.wasm', import.meta.url);
+        input = new URL('exify-78da2ec99161c7d_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
